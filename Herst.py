749f68d2e909c460be748ea14b6ae8cb89d55a01
PPeachPie <56588811+PPeachPie@users.noreply.github.com>
2020-10-07 10:00:17 +1100

    Update Herst.py

--- Herst.py
+++ Herst.py
@@ -29,28 +29,26 @@ def CKO(tau, sumaverage):
 # Метод наименьших квадратов
 def LS(x, y):
     t=len(x)
-    medX=0
-    medY=0
-    medXX=0
     medXY=0
-    for i in x: medX+=math.log(i)
-    for i in y: medY+=math.log(i)
-    for i in x: medXX+=np.power(math.log(i), 2)
+    medX=sum([math.log(c) for c in x])
+    medY=sum([math.log(c) for c in y])
+    medXX=sum([np.power(math.log(c), 2) for c in x])
     for i in range(len(x)): medXY+=math.log(x[i])*math.log(y[i])
     b=(t*medXY-medX*medY)/(t*medXX-np.power(medX, 2))
     a=math.exp((medY-b*medX)/t)
-    return a
+    return [a, b]
 
 
-def PowFx(a, b, x):
-    return a*np.power(x, b)
+def LinearFx(x):
+    return a+b*x 
+
 
 x=[2.5, 3,3.3,4,4.4,5,5.5,6,6.5,7,7.5,8,8.5]
 y=[4.4,3.6,3,2.7,2.1,1.8,1.9,1.5,1.4,1.3,1.2,1.1,1.1]
 a=LS(x, y)[0]
 b=LS(x, y)[1]
 plt.scatter(x, y, c="red")
-for i in range(20):
-    plt.plot(i, LinearFx(a, b, i))
+ln=[LinearFx(c) for c in x]
+plt.plot(x, ln)
     # test value LS([0.1, 0.2, -0.1, 0.5, 0.002], [0, 1, 2, 3, 4 ])
 # test value from site mathhelp planet x=[2.5, 3,3.3,4,4.4,5,5.5,6,6.5,7,7.5,8,8.5] y=[4.4,3.6,3,2.7,2.1,1.8,1.9,1.5,1.4,1.3,1.2,1.1,1.1]
