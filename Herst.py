50f26ecd708351405080daefbbf254b9fe55ae40
PPeachPie <56588811+PPeachPie@users.noreply.github.com>
2020-10-08 12:10:49 +1100

    Update Herst.py

--- Herst.py
+++ Herst.py
@@ -10,7 +10,7 @@ def Average(tau, val):
     return res
 
 
-def SumAverage_Scope(tau, val):
+def Range(tau, val):
     sum=0
     ave = Average(tau, val)
     min=val[1]-ave
@@ -19,11 +19,10 @@ def SumAverage_Scope(tau, val):
         sum += i - ave
         if sum<min: min = sum
         if sum>max: max = sum
-    CKO(tau, sum)
     return max-min
 
 
-def CKO(tau, sumaverage):
+def SCO(tau, sumaverage):
     return np.power(np.power(sumaverage, 2)/(tau - 1), 0.5)
 
 # Метод наименьших квадратов
@@ -43,15 +42,24 @@ def LinearFx(x):
     return a+b*x 
 
 
-y=[0.2, -1.3, -0.003, 0.1]
+y=[4.4,3.6,3,2.7,2.1,1.8,1.9,1.5,1.4,1.3,1.2,1.1,1.1]
 tau0=2
 delta_tau=2
 x=[i+1 for i in range(len(y))]
 a=LS(x, y)[0]
 b=LS(x, y)[1]
-plt.scatter(x, y, c="red")
+
 ln=[LinearFx(c) for c in x]
+
+plt.figure(figsize=(5, 5))
+plt.subplot(2,1,1)
+plt.scatter(x, y, c="red")
 plt.plot(x, ln)
-plt.plot()
+plt.subplot(2,1,2)
+nowtau=tau
+while nowtau<len(y):
+    defR=Range(nowtau, y)
+    defS=SCO(nowtau, y)
+    plt.plot(math.log(x), math.log(defR/defS))
     # test value LS([0.1, 0.2, -0.1, 0.5, 0.002], [0, 1, 2, 3, 4 ])
 # test value from site mathhelp planet x=[2.5, 3,3.3,4,4.4,5,5.5,6,6.5,7,7.5,8,8.5] y=[4.4,3.6,3,2.7,2.1,1.8,1.9,1.5,1.4,1.3,1.2,1.1,1.1]
