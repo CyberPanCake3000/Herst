0f20da5211ab9d6c9f3d868ba15c58ead86cb829
PPeachPie <56588811+PPeachPie@users.noreply.github.com>
2020-11-19 00:15:44 +1000

    Update Herst.py

--- Herst.py
+++ Herst.py
@@ -1,75 +1,110 @@
 import numpy as np
 import math
 import matplotlib.pyplot as plt
+import time
+# np.seterr(divide='ignore', invalid='ignore')
 
-def Average(tau, val):
+
+def average(tau, val):
+    # Вычисление среднего значения для текущего tau
+    return (sum(val)/tau)
+
+
+def rrange(tau, val):
+    # Вычисление размаха для текущего tau
     sum=0
-    for i in val:
-        sum += i
-    res = sum/tau
-    return res
-
-
-def Range(tau, val):
-    sum=0
-    ave = Average(tau, val)
-    min=val[1]-ave
-    max=val[1]-ave
+    ave = average(tau, val)
+    min=val[0]-ave
+    max=val[0]-ave
     for i in val:
         sum += i - ave
         if sum<min: min = sum
         if sum>max: max = sum
-    return max-min
+    return (max-min)
 
 
-def SCO(tau, sumaverage):
-    return np.power(np.power(sumaverage, 2)/(tau - 1), 0.5)
+def SCO(tau, val):
+    res = 0
+    medium = sum(val)/tau
+    s = 0
+    for i in val:
+        # s+=np.power(i-medium, 2)
+        s+=(i - medium)**2
+
+    res=(s / (tau - 0.99999999))**0.5
+    return res
+    # return np.power(np.power(sumaverage, 2)/(tau - 1), 0.5)
+
+
+def Fx(x):
+    return a*(b**x)
+
 
 # Метод наименьших квадратов
+
 def LS(x, y):
-    t=len(x)
-    medXY=0
-    medX=sum([math.log(c) for c in x])
-    medY=sum([math.log(c) for c in y])
-    medXX=sum([np.power(math.log(c), 2) for c in x])
-    for i in range(len(x)): medXY+=math.log(x[i])*math.log(y[i])
-    b=(t*medXY-medX*medY)/(t*medXX-np.power(medX, 2))
-    a=math.exp((medY-b*medX)/t)
+    # sX = sum([math.log(i) for i in x])
+    sX=1
+    sY = 1
+    sXY = 1
+    sXX = 1
+    n = len(y)
+    for i in x:
+        sX += math.log(i)
+        # sXX += np.power(math.log(i), 2)
+        sXX += math.log(i)**2
+    for i in y:
+        if (i > 0):
+            sY += math.log(i)
+
+    for i in range(len(y)):
+        if (y[i] > 0):
+            sXY += math.log(x[i]) * math.log(y[i])
+
+    b = (n * sXY - sX * sY) / (n * sXX - sX**2)
+    # np.power(sX, 2)
+    a = math.exp((sY - b * sX) / n)
     return [a, b]
 
 
-def LinearFx(x):
-    return a+b*x 
+f = open(r"E:\test2.txt")
+fromfile = f.readlines()
+f.close()
+tau = 1
+delta_tau = 1
+
+y = np.array([float(i) for i in fromfile])
+
+# plt.plot(y)
+new_y=[]
+x=[]
+# x=[i+1 for i in range(len(y))]
+# x=np.array([i+1 for i in range(len(y))])
+# t=time.time()
+# for i in range(1, len(y), delta_tau):
+for i in np.arange(1, len(y), delta_tau):
+    def_R=rrange(i, y[:i])
+    def_S=SCO(i, y[:i])
+    if math.isnan(def_R / def_S): new_y.append(0)
+    else: new_y.append(def_R / def_S)
+    x.append(i)
+# print('lol', time.time()-t)
+
+new_new_y=np.array(new_y)
+print(new_new_y[0], x[0])
+# new_x=np.array(x)
+a=LS(x, new_new_y)[0]
+b=LS(x, new_new_y)[1]
+print(round(b, 4))
+print(round(2-b, 4)) #фрактальная размерность
+print(round((1/b), 4))#размерность Мандельброта
+print(np.power(2, 2*b-1)-1) #корреляционное соотношение
+res_g=[]
+for i in x:
+    res_g.append(Fx(i))
+
+plt.plot(res_g)
+plt.show()
 
 
-f=open(r"C:\Users\student_02\Desktop\test.dat")
-fromfile=f.readlines()
-y=[float(i) for i in fromfile]
-tau=2
-delta_tau=2
-x=[i+1 for i in range(len(y))]
-a=LS(x, y)[0]
-b=LS(x, y)[1]
 
-# plt.figure(figsize=(5, 5))
-# plt.subplot(2,1,1)
-# plt.scatter(x, y, c="red")
-# plt.plot(x, ln)
-# plt.subplot(2,1,2)
-nowtau=tau
-
-
-PointList=[]
-pList=[]
-newl=[]
-
-while nowtau<len(y):
-    defR=Range(nowtau, y)
-    defS=SCO(nowtau, Average(nowtau, y))
-    PointList.append(math.log(nowtau))
-    pList.append(math.log(defR / defS))
-    newl.append(LinearFx(math.log(nowtau)))
-    nowtau+=delta_tau
-
-plt.plot(pList, PointList)
-plt.plot(newl)
